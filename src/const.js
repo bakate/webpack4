5ec9042bf9f50838fe6a8dd6c78db236792a8b37
bakate <bakateba@gmail.com>
2019-09-11 20:42:18 +0200

    a new function added into const.js

--- src/const.js
+++ src/const.js
@@ -1,2 +1,6 @@
 const name = 'Bakate';
+const extract = [1, 2, 3, 4, 5, 6, 7];
+const odd = extract.filter(val => val % 2 === 0);
+
 export default name;
+export { odd };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import name from './const';
+import name, { odd } from './const';
 
 console.log('Hello Bakate how are you doing papi');
 console.log(` My name is ${name}`);
+console.log(` Here we go, that's what you're looking for: ${odd}`);
