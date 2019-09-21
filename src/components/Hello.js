f73455f8ba80690e626c160c301f40b0bf02512b
bakate <bakateba@gmail.com>
2019-09-21 18:50:29 +0200

    I've better understood typeScript and rendered something with "react-intl"

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,4 +1,23 @@
 import React from 'react';
+import { IntlProvider, FormattedMessage } from 'react-intl';
 
-const Hello = () => <p>Hello from a React Component</p>;
+const messages = {
+  simple: 'Bonjour Monsieur',
+  placeholder: 'Bonjour {name}, comment vas-tu?',
+  date: 'Bonjour, nous sommes en {date}',
+  time: 'Il est actuellement {time} à Paris',
+};
+
+const Hello = () => (
+  <IntlProvider locale="fr" messages={messages}>
+    <>
+      <FormattedMessage id="simple" />
+      <br />
+      <FormattedMessage id="placeholder" values={{ name: 'Tony' }} />
+      <br />
+      <FormattedMessage id="date" values={{ date: new Date().getFullYear() }} />
+      <br />
+    </>
+  </IntlProvider>
+);
 export default Hello;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Hello from './components/Hello';
-// import name, { even } from './const';
 
-// console.log('Hello Bakate how are you doing papi');
-// console.log(` My name is ${name}`);
-// console.log(` Here we go: ${even}`);
-ReactDom.render(
-  <IntlProvider locale={usersLocale} messages={translationsForUsersLocale}>
-    <Hello />
-  </IntlProvider>,
-  document.getElementById('container')
-);
+ReactDom.render(<Hello />, document.getElementById('hi'));
